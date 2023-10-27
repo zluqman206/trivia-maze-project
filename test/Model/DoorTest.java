4535e7b8bb3abafa1dc92742423a5b1e62834f96
issaice <hawo1551issa@gmail.com>
2023-10-27 16:10:27 -0700

    Updated project structure. Project is now in MVC format

---
+++ test/Model/DoorTest.java
@@ -0,0 +1,18 @@
+package Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isLocked() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lock() {
+    }
+}
